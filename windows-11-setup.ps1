9903da6b8df2a8cc710576552ee28fc50a82c1e3
Timothy Allen <tallen@wharton.upenn.edu>
2022-04-17 12:38:50 -0400

    Update windows-11-setup.ps1

--- windows-11-setup.ps1
+++ windows-11-setup.ps1
@@ -3,6 +3,10 @@
 
 # Uninstall the crapware that comes with Windows 10 - leave the Store
 Write-Output("Uninstalling all the packaged crapware; we will leave Windows Store so anything can be replaced...")
+
+# Get-AppxPackage *SpotifyAB.SpotifyMusic* | Remove-AppxPackage
+# Get-AppxPackage *Disney* | Remove-AppxPackage
+
 DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename | % {$_ -replace("PackageName : ", "")} | select-string "^((?!WindowsStore).)*$" | ForEach-Object {Remove-AppxPackage -allusers -package $_}
 
 # Finally, stop and restart explorer.
