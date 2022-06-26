f52817bab71f0f461b8809b130dea0853b2b8960
Timothy Allen <tallen@wharton.upenn.edu>
2022-06-26 19:40:41 -0400

    Update windows-11-setup.ps1

--- windows-11-setup.ps1
+++ windows-11-setup.ps1
@@ -1,15 +1,17 @@
 # This script sets up Windows 11 the way I like it.
 # It should be run in Administrator mode from PowerShell.
 
-# Uninstall the crapware that comes with Windows 10 - leave the Store
 Write-Output("Uninstalling all the packaged crapware; we will leave Windows Store so anything can be replaced...")
+DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename | % {$_ -replace("PackageName : ", "")} | select-string "^((?!WindowsStore).)*$" | ForEach-Object {Remove-AppxPackage -allusers -package $_}
 
 # Get-AppxPackage *SpotifyAB.SpotifyMusic* | Remove-AppxPackage
 # Get-AppxPackage *Disney* | Remove-AppxPackage
 
-DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename | % {$_ -replace("PackageName : ", "")} | select-string "^((?!WindowsStore).)*$" | ForEach-Object {Remove-AppxPackage -allusers -package $_}
+Write-Output("Installing Winget package manager...")
+Invoke-WebRequest https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile Winget.msi
+msiexec /i Winget.msi /qn /norestart
 
-# Uninstall other stuff with winget
+Write-Output("Uninstalling crap we probably don't want, like Teams and OneDrive...")
 winget uninstall onedrive
 
 # Finally, stop and restart explorer.
