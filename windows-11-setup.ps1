fe792f59f539e12b8de97ee3b64d0ebf9b9d29a7
Timothy Allen <tallen@wharton.upenn.edu>
2022-06-29 14:21:08 -0400

    Update windows-11-setup.ps1

--- windows-11-setup.ps1
+++ windows-11-setup.ps1
@@ -4,16 +4,13 @@
 Write-Output("Uninstalling all the packaged crapware; we will leave Windows Store so anything can be replaced...")
 DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename | % {$_ -replace("PackageName : ", "")} | select-string "^((?!WindowsStore).)*$" | ForEach-Object {Remove-AppxPackage -allusers -package $_}
 
-# Get-AppxPackage *SpotifyAB.SpotifyMusic* | Remove-AppxPackage
-# Get-AppxPackage *Disney* | Remove-AppxPackage
-
 Write-Output("Installing Winget package manager...")
 Invoke-WebRequest https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile ".\VCLibs.appx"
 add-appxpackage -Path ".\VCLibs.appx"
 Invoke-WebRequest https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile ".\Winget.msixbundle"
 add-appxpackage -Path ".\Winget.msixbundle"
 
-Write-Output("Uninstalling crap we probably don't want, like Teams and OneDrive...")
+Write-Output("Uninstalling more crap we probably don't want, like apps for OneDrive, Spotify, and Disney+...")
 winget uninstall "Spotify Music"
 winget uninstall "Disney+"
 winget --accept-package-agreements uninstall onedrive
