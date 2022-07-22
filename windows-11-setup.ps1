627109de36615d6cdc15ba7a64d23af2adab57a5
Timothy Allen <tallen@wharton.upenn.edu>
2022-07-22 15:45:18 -0400

    Install winget.

--- windows-11-setup.ps1
+++ windows-11-setup.ps1
@@ -7,10 +7,11 @@ DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename | % {$_ -r
 
 # winget is now installed by default with the latest Windows Updates! Yay!
 # Write-Output("Installing Winget package manager...")
-# Invoke-WebRequest https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile ".\VCLibs.appx"
-# add-appxpackage -Path ".\VCLibs.appx"
-# Invoke-WebRequest https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile ".\Winget.msixbundle"
-# add-appxpackage -Path ".\Winget.msixbundle"
+Add-AppxPackage 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+Invoke-WebRequest https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.1 -OutFile ".\UI.Xaml.zip"
+Expand-Archive .\UI.Xaml.zip -DestinationPath ".\uixaml"
+Add-AppxPackage -Path ".\uixaml\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx"
+Add-AppxPackage 'https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
 
 Write-Output("Uninstalling more crap we probably don't want, like apps for OneDrive, Spotify, and Disney+...")
 winget uninstall "Cortana" --silent --accept-source-agreements
