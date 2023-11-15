d9fe1b357fd736f265f73958bf5e71ec372d4740
Timothy Allen <tallen@wharton.upenn.edu>
2023-11-14 19:56:17 -0500

    Update windows-11-setup.ps1

--- windows-11-setup.ps1
+++ windows-11-setup.ps1
@@ -66,7 +66,8 @@ Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentD
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager' -Name 'SubscribedContent-88000326Enabled' -Value 0
 
 # Stop pestering to create a Microsoft Account. Local accounts: this is the way.
-New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'NoConnectedUser' -PropertyType DWord -Value 3 -f
+New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Force
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'NoConnectedUser' -PropertyType DWord -Value 3 -Force
 
 # Get rid of the incredibly stupid "Show More Options" context menu default that NO ONE ASKED FOR
 New-Item -Path 'HKCU:\Software\Classes\CLSID' -Name '{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}' -f
